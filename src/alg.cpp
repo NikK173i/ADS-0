7ccfe02899a447b285bc51865baa8ec4cb2c88e8
NikK173i <160059242+NikK173i@users.noreply.github.com>
2024-02-22 16:55:12 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,20 +4,20 @@ using std::cout;
 using std::endl;
 
 int gcd(int a, int b) {
-	int c = 1;
-	int z = (a > b) ? a : b;
-	for (int i = 1; i <= z; ++i) {
-		if ((a % i == 0 && b % i == 0)) {
-			c = i;
-		}
-	}
-	return c;
+    int c = 1;
+    int z = (a > b) ? a : b;
+    for (int i = 1; i <= z; ++i) {
+        if ((a % i == 0 && b % i == 0)) {
+            c = i;
+        }
+    }
+    return c;
 }
 
 int main() {
-	int a, b;
-	std::cin >> a;
-	std::cin >> b;
-	cout << gcd(a, b) << endl;
-	return 0;
+    int a, b;
+    std::cin >> a;
+    std::cin >> b;
+    cout << gcd(a, b) << endl;
+    return 0;
 }
